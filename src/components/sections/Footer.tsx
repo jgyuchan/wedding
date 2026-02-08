75748dbd7d5d761b4ff5728629c0899e5ba9c60b
jgyuchan <43114271+jgyuchan@users.noreply.github.com>
2026-02-08 15:39:57 +0900

    Update Footer.tsx

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Footer = () => (
   <FContainer>
     <p>© 2026 gyuchan and hayoung</p>
-    <p style={{fontSize: '0.75rem', color: '#aaa'}}>Made with ❤️</p>
+    <p style={{fontSize: '0.75rem', color: '#aaa', marginTop: '0.5rem'}}>Made with ❤️</p>
   </FContainer>
 );
 const FContainer = styled.footer` padding: 4rem 1.5rem; text-align: center; border-top: 1px solid #f0f0f0; color: #888; font-size: 0.875rem; `;
