5748c3a9e4597856a91a58997e1197e98c263dbe
jgyuchan <43114271+jgyuchan@users.noreply.github.com>
2026-02-08 15:53:59 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({ children }: { children: React.ReactNode; })
       <head><meta name="generator" content="Wedding-Invitation" /></head>
       <body>
         <GlobalStyle /><CacheManager />
-        {/* 노래 주소를 절대 주소로 지정합니다. 터치 후 재생됩니다 */}
+        {/* 노래 주소를 전체 주소로 지정합니다. 터치 후 재생됩니다 */}
         <audio src="https://jgyuchan.github.io/wedding/bgm.mp3" autoPlay loop style={{ display: 'none' }} />
         {children}
       </body>
