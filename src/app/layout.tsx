a7a8bb4e5c3802ac3037a1366bf705784add5a06
jgyuchan <43114271+jgyuchan@users.noreply.github.com>
2026-02-08 15:26:14 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,19 +5,10 @@ import CacheManager from '../components/CacheManager';
 export default function RootLayout({ children }: { children: React.ReactNode; }) {
   return (
     <html lang="ko">
-      <head>
-        <meta name="generator" content="Wedding-Invitation" />
-      </head>
+      <head><meta name="generator" content="Wedding-Invitation" /></head>
       <body>
-        <GlobalStyle />
-        <CacheManager />
-        {/* 노래 주소를 전체 주소로 지정하여 경로 문제를 원천 해결합니다. */}
-        <audio 
-          src="https://jgyuchan.github.io/wedding/bgm.mp3" 
-          autoPlay 
-          loop 
-          style={{ display: 'none' }} 
-        />
+        <GlobalStyle /><CacheManager />
+        <audio src="https://jgyuchan.github.io/wedding/bgm.mp3" autoPlay loop style={{ display: 'none' }} />
         {children}
       </body>
     </html>
