7a9dd8dd5a3bfc4267783476c416dddea7225f92
jgyuchan <43114271+jgyuchan@users.noreply.github.com>
2026-02-08 16:06:28 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,7 @@ const nextConfig = {
   basePath: '/wedding',
   assetPrefix: '/wedding',
   output: 'export',
-  compiler: {
-    styledComponents: true,
-  },
-  images: {
-    unoptimized: true, // 깃허브 페이지에서 이미지가 안 나오는 문제를 해결합니다.
-  },
+  compiler: { styledComponents: true },
+  images: { unoptimized: true },
 };
-
 export default nextConfig;
