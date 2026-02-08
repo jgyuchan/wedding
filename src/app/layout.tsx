9dc61878dfa289b5a14f82ec96f7f39dba10f74b
jgyuchan <43114271+jgyuchan@users.noreply.github.com>
2026-02-08 15:16:38 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,18 @@
 import React from 'react';
-import { weddingConfig } from '../config/wedding-config';
-import Watermark from '../lib/watermark';
 import { GlobalStyle } from '../styles/globalStyles';
 import CacheManager from '../components/CacheManager';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode; }) {
   return (
     <html lang="ko">
       <head>
-        <link rel="preload" href="/fonts/PlayfairDisplay-Italic.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
-        <link rel="preload" href="/fonts/MaruBuri-Regular.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
         <meta name="generator" content="Wedding-Invitation" />
       </head>
       <body>
         <GlobalStyle />
         <CacheManager />
-        
-        {/* 노래 주소를 전체 인터넷 주소로 박아넣어 경로 문제를 해결합니다. */}
-        <audio 
-          src="https://jgyuchan.github.io/wedding/bgm.mp3" 
-          autoPlay 
-          loop 
-          style={{ display: 'none' }} 
-        />
-
-        <Watermark />
+        {/* 브라우저 터치 후 재생됨 */}
+        <audio src="./bgm.mp3" autoPlay loop style={{ display: 'none' }} />
         {children}
       </body>
     </html>
