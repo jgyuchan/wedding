4d27e15dcfdac45b10eb424400a8c2c6da37e9f2
jgyuchan <43114271+jgyuchan@users.noreply.github.com>
2026-02-09 17:01:37 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,8 @@ export default function RootLayout({ children }: { children: React.ReactNode; })
       <head><meta name="generator" content="Wedding-Invitation" /></head>
       <body>
         <GlobalStyle /><CacheManager />
-        {/* [정답] bgm.mp3는 public 폴더 바로 아래에 있으므로 images를 빼야 합니다! */}
-        <audio src="/wedding/bgm.mp3?v=finish" autoPlay loop style={{ display: 'none' }} />
+        {/* [무적 치트키] 노래 경로도 인터넷 전체 주소로 변경 */}
+        <audio src="https://jgyuchan.github.io/wedding/bgm.mp3" autoPlay loop style={{ display: 'none' }} />
         {children}
       </body>
     </html>
